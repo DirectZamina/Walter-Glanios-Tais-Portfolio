ea76a2e15aaf6155ff4ddca11c0c15422b9a35b2
DirectZamina <85369870+DirectZamina@users.noreply.github.com>
2025-05-07 17:46:14 +0200

    update navbar

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ document.getElementById('btn').addEventListener('click',() => {
     document.body.classList.toggle('dark');
     document.body.classList.toggle('light');
 })
+
+document.querySelector('.burger').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
